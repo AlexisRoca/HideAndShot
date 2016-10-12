26ba04d61a4bc0e8ce0bec99b74a26e31faf5c45
Goudé <goude.ific@outlook.sa>
2016-10-12 15:42:52 +0200

    [GAME] Agent structure + Leader behavior

---
+++ Assets/Agent/Scripts/Agent.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Agent : MonoBehaviour {
+
+    // Attributes deffinition (get/set)
+    public int _mass { get; protected set; }
+    public int _maxSpeed { get; protected set; }
+    public int _maxSteer { get; protected set; }
+
+    public float _orientation { get; protected set; }
+    public Vector2 _position { get; protected set; }
+    public Vector2 _velocity { get; protected set; }
+
+
+    // Use this for initialization
+    void Start () {
+        defineAgent();
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+        // Compute steering force under  max steer
+        Vector2 steerForce = Vector2.ClampMagnitude(steering(), _maxSteer);
+
+        // Compute acceleration
+        Vector2 acceleration = steerForce / _mass;
+
+        // Compute new velocity
+        Vector2 newVelocity = _velocity + acceleration * Time.deltaTime;
+
+        // Compute new orientation
+        _orientation += Mathf.Acos(Vector2.Dot(_velocity, newVelocity)) % 360.0f;
+
+        // Apply new velocity under a max speed
+        _velocity = Vector2.ClampMagnitude(newVelocity, _maxSpeed);
+
+        // Compute new position
+        _position += _velocity * Time.deltaTime;
+
+
+        // Apply changes to the GameObject
+        updateGameObject();
+	}
+
+
+    // Update the agent in the game world
+    void updateGameObject () {
+        transform.position = new Vector3(_position.x, transform.position.y, _position.y);
+    }
+
+
+    // Define the agent properties
+    virtual protected void defineAgent () {
+        _mass = 0;
+        _maxSpeed = 0;
+        _maxSteer = 0;
+        _orientation = 0.0f;
+        _position = Vector2.zero;
+        _velocity = Vector2.zero;
+    }
+
+
+    // Reimplemented function for different comportment
+    virtual protected Vector2 steering () {
+        return Vector2.zero;
+    }     
+}
---
+++ Assets/Agent/Scripts/Leader.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Leader : Agent {
+    
+    // Define the wander circle behavior
+    public int radiusCircle = 0;
+    public float variation = 0.0f;
+
+    // Define the random point position around the circle
+    private float anglePoint = 0.0f;
+
+    // Define the agent properties
+    override protected void defineAgent()
+    {
+        _mass = 10;
+        _maxSpeed = 50;
+        _maxSteer = 1000;
+        _orientation = 0.0f;
+        _position = new Vector2(250,250);
+        _velocity = Vector2.zero;
+    }
+
+
+    // Reimplemented function for different behavior
+    override protected Vector2 steering()
+    {
+        anglePoint = (anglePoint + (Random.Range(-1.0f, 1.0f)) * variation) % (2 * Mathf.PI);
+
+        Vector2 force = new Vector2(Mathf.Cos(anglePoint), Mathf.Sin(anglePoint)) * radiusCircle;
+
+        return force;
+    }
+
+
+    // Force Visualization
+    void OnDrawGizmosSelected()
+    {
+        Vector2 circlePosition = _position + _velocity.normalized * 1.5f * radiusCircle;
+        Vector2 pointPosition = circlePosition + new Vector2(Mathf.Cos(anglePoint), Mathf.Sin(anglePoint)) * radiusCircle;
+
+        Vector2 force = new Vector2(Mathf.Cos(anglePoint), Mathf.Sin(anglePoint)) * radiusCircle;
+
+        Vector3 circlePosition3D = new Vector3(circlePosition.x, 0, circlePosition.y);
+        Vector3 pointPosition3D = new Vector3(pointPosition.x, 0, pointPosition.y);
+        Vector3 agentPosition3D = new Vector3(_position.x, 0, _position.y);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(circlePosition3D, radiusCircle);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(pointPosition3D, 1);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(circlePosition3D, pointPosition3D);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(agentPosition3D, circlePosition3D);
+        Gizmos.DrawLine(agentPosition3D, pointPosition3D);
+
+    }
+}
