51bc41b8f35e8207f43add0a8de07bfc0346be86
Alexis Roca <alexis.roca@hotmail.fr>
2016-10-13 16:18:11 +0200

    [Cursor] add primitive mouse cursor

---
+++ Assets/Shooter/Cursor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursor : MonoBehaviour {
+
+    public 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 1000))
+            {
+                if(hit.collider.GetComponent<Agent>())
+                {
+                    print("Hit something!");
+                    Debug.DrawLine(ray.origin, hit.point);
+                }
+            }
+        }
+    }
+}
