c01d84a10e71ae470b7a0fbaa0e2184f8964a6b4
Alexis Roca <alexis.roca@hotmail.fr>
2016-10-13 19:22:22 +0200

    [Cursor] add real cursor

--- Assets/Shooter/Cursor.cs
+++ Assets/Shooter/Cursor.cs
@@ -1,29 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Cursor : MonoBehaviour {
-
-    public 
+public class Cursor : MonoBehaviour
+{
+    public Sprite m_target;
+    public GameObject m_cursor;
 
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update ()
+	void Start ()
     {
-        if(Input.GetMouseButtonDown(0))
+        m_target = Resources.Load<Sprite>("Sprites/Targets/redSniperTarget");
+
+        m_cursor = new GameObject("Cursor");
+        SpriteRenderer renderer = m_cursor.AddComponent<SpriteRenderer>();
+        renderer.sprite = m_target;
+
+        m_cursor.transform.position = new Vector3(0, 150, 0);
+        m_cursor.transform.localScale = new Vector3(3, 3, 3);
+        m_cursor.transform.rotation = Camera.main.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Vector2 speed = Vector2.zero;
+
+        speed += (Input.GetKey(KeyCode.UpArrow)) ? new Vector2(0, 1) : Vector2.zero;
+        speed += (Input.GetKey(KeyCode.DownArrow)) ? new Vector2(0, -1) : Vector2.zero;
+        speed += (Input.GetKey(KeyCode.LeftArrow)) ? new Vector2(-1, 0) : Vector2.zero;
+        speed += (Input.GetKey(KeyCode.RightArrow)) ? new Vector2(1, 0) : Vector2.zero;
+
+        m_cursor.transform.position = m_cursor.transform.position + new Vector3(speed.x,0,speed.y) * 30 * Time.deltaTime;
+
+
+        if(Input.GetKey(KeyCode.Keypad0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = new Ray(Camera.main.transform.position,(m_cursor.transform.position - Camera.main.transform.position).normalized);
             RaycastHit hit;
 
-            if(Physics.Raycast(ray, out hit, 1000))
+            float rayLenght = 100000;
+            Debug.DrawLine(ray.origin, ray.direction * rayLenght);
+
+            if(Physics.Raycast(ray, out hit, rayLenght))
             {
                 if(hit.collider.GetComponent<Agent>())
                 {
-                    print("Hit something!");
-                    Debug.DrawLine(ray.origin, hit.point);
+                    if (hit.collider.GetComponent<PlayerHide>())
+                        print("Hit Player!");
+                    else
+                        print("Hit Agent, Try Again");
                 }
             }
         }
