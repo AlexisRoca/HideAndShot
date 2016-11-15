7e5cc4edee74e65f65caade0878fc8e17154aea4
Alexis Roca <alexis.roca@hotmail.fr>
2016-11-15 11:28:48 +0100

    [Cursor] Move cursor to canvas

--- Assets/Shooter/Cursor.cs
+++ Assets/Shooter/Cursor.cs
@@ -10,15 +10,19 @@ public class Cursor : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-        m_target = Resources.Load<Sprite>("Sprites/Targets/redSniperTarget");
+        GameObject canvas = GameObject.FindGameObjectWithTag("Canvas");
 
+        m_target = Resources.Load<Sprite>("Sprites/Targets/redSniperTarget");
         m_cursor = new GameObject("Cursor");
+
         SpriteRenderer renderer = m_cursor.AddComponent<SpriteRenderer>();
         renderer.sprite = m_target;
 
         m_cursor.transform.position = new Vector3(250, 150, 250);
         m_cursor.transform.localScale = new Vector3(3, 3, 3);
         m_cursor.transform.rotation = Camera.main.transform.rotation;
+
+        m_cursor.transform.SetParent(canvas.transform);
     }
 
     // Update is called once per frame
@@ -31,10 +35,10 @@ public class Cursor : MonoBehaviour
         speed += (Input.GetKey(KeyCode.LeftArrow)) ? new Vector2(-1, 0) : Vector2.zero;
         speed += (Input.GetKey(KeyCode.RightArrow)) ? new Vector2(1, 0) : Vector2.zero;
 
-        m_cursor.transform.position = m_cursor.transform.position + new Vector3(speed.x,0,speed.y) * m_sensibility * Time.deltaTime;
+        m_cursor.transform.localPosition = m_cursor.transform.localPosition + new Vector3(speed.x,speed.y,0.0f) * m_sensibility * Time.deltaTime;
 
 
-        if(Input.GetKey(KeyCode.Keypad0))
+        if(Input.GetKey(KeyCode.Space))
         {
             Ray ray = new Ray(Camera.main.transform.position,(m_cursor.transform.position - Camera.main.transform.position).normalized);
             RaycastHit hit;
